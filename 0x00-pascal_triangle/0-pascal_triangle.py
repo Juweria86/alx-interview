d98917e956b10b97a91efe175815b5ffcfc66160
Juweria86 <jujumelmi143@gmail.com>
2024-06-30 22:54:59 +0300

    pascals triangle implementation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,7 +10,7 @@ def pascal_triangle(n):
         return []
     result = [[1]]
     for i in range(1, n):
-        temp = [0] + result[-1] +[0]
+        temp = [0] + result[-1] + [0]
         row = []
         for j in range(len(result[-1]) + 1):
             row.append(temp[j] + temp[j + 1])
